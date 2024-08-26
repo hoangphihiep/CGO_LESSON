f9ff2cbe5fed5d41824cca3083da7e50a83e8d8d
hoangphihiep <phihiep31012004@gmail.com>
2024-08-26 14:29:36 +0700

    init unity project

---
+++ Assets/Text.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Text : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("Log");
+        Debug.LogWarning("Warning Log");
+        Debug.LogError("Error");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
